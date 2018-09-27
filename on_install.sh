890d59feb4fd5d67c09adc9d1279b232811cebd3
Conrad <conrad.heidebrecht@gmail.com>
2018-09-27 12:32:51 -0700

    updated install script

--- on_install.sh
+++ on_install.sh
@@ -1,7 +1,10 @@
-#!/bin/zsh
+#!/bin/bash
 
 HOMEDIR='/home/'$1
 CURDIR=`pwd`
+
+su - $1
+
 rm $HOMEDIR/.zshrc
 rm $HOMEDIR/.zsh_aliases
 rm $HOMEDIR/.tmux.conf
@@ -9,6 +12,30 @@ rm -rf $HOMEDIR/.config/i3
 rm -r $HOMEDIR/.screenlayout
 rm -r $HOMEDIR/.config/nvim
 mkdir -p $HOMEDIR/.config/nvim/plugged
+mkdir -p $HOMEDIR/.config/i3
+
+sudo apt install -y git build-essential xclip python3-pip curl zsh tmux i3 nitrogen apt-transport-https
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+sudo apt update
+sudo apt install -y docker-ce
+sudo groupadd docker
+sudo usermod -aG docker $1
+su - $1
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+git clone https://github.com/bhilburn/powerlevel9k.git $HOMEDIR/.oh-my-zsh/custom/themes/powerlevel9k
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+curl -sL https://deb.nodesource.com/setup_10.x | -E bash -
+sudo apt install -y neovim yarn nodejs
+curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \\
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+sudo apt install -y arandr i3blocks pm-utils xbacklight blueman mongodb xautolock fonts-firacode
+sudo apt install -f -y
+
 ln -s $CURDIR/.config/nvim/init.vim $HOMEDIR/.config/nvim/init.vim
 ln -s $CURDIR/.config/nvim/colors $HOMEDIR/.config/nvim/colors
 ln -s $CURDIR/.zshrc $HOMEDIR/.zshrc
@@ -18,30 +45,10 @@ ln -s $CURDIR/.screenlayout $HOMEDIR/.screenlayout
 ln -s $CURDIR/.config/i3/config $HOMEDIR/.config/i3/config
 ln -s $CURDIR/.config/i3/startup $HOMEDIR/.config/i3/startup
 
-apt install -y git build-essential xclip python3-pip curl zsh tmux i3 nitrogen apt-transport-https
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
-add-apt-repository \
-   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-   $(lsb_release -cs) \
-   stable"
-apt update
-apt install -y docker-ce
-groupadd docker
-usermod -aG docker $1
-su - $1
-sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-git clone https://github.com/bhilburn/powerlevel9k.git $HOMEDIR/.oh-my-zsh/custom/themes/powerlevel9k
-curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add -
-echo "deb https://dl.yarnpkg.com/debian/ stable main" | tee /etc/apt/sources.list.d/yarn.list
-curl -sL https://deb.nodesource.com/setup_10.x | -E bash -
-apt install -y neovim yarn nodejs
-curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \\
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-apt install -y arandr i3blocks pm-utils xbacklight blueman mongodb xautolock fonts-firacode
-apt install -f -y
 cd ~/Downloads
-dpkg -i google-chrome-stable_current_amd64.deb
-dpkg -i skypeforlinux-64.deb
+sudo dpkg -i google-chrome-stable_current_amd64.deb
+sudo dpkg -i skypeforlinux-64.deb
+sudo apt install -f
 mkdir ~/applications
 cd ~/applications
 git clone https://github.com/flutter/flutter.git -b beta
@@ -58,9 +65,9 @@ git clone https://github.com/eternali/tictacthrow
 mkdir ~/VueProjects
 cd ~/VueProjects
 git clone https://github.com/eternali/conradheidebrecht.com
-apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 931FF8E79F0876134EDDBDCCA87FF9DF48BF1C90
-echo deb http://repository.spotify.com stable non-free | tee /etc/apt/sources.list.d/spotify.list
-apt install -y spotify-client
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 931FF8E79F0876134EDDBDCCA87FF9DF48BF1C90
+echo deb http://repository.spotify.com stable non-free | sudo tee /etc/apt/sources.list.d/spotify.list
+sudo apt install -y spotify-client
 git config --global user.email "conrad.heidebrecht@gmail.com"
 git config --global user.name "Conrad"
 cd ~/Downloads
