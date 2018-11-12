3f24f2eebb77d9ecec1e04a8c8effc2543a472d4
Conrad <conrad.heidebrecht@gmail.com>
2018-11-12 10:49:38 -0500

    updated screenlayouts

--- .screenlayout/equibit06.sh
+++ .screenlayout/equibit06.sh
@@ -1,2 +1,9 @@
 #!/bin/sh
-xrandr --output VIRTUAL1 --off --output eDP1 --primary --mode 1920x1080 --pos 1440x1432 --rotate normal --output DP1 --off --output HDMI2 --off --output HDMI1 --off --output DP1-3 --off --output DP1-2 --mode 2560x1440 --pos 2416x0 --rotate normal --output DP1-1 --mode 2560x1440 --pos 0x0 --rotate right --output DP2 --off
+xrandr --output VIRTUAL1 --off
+xrandr --output eDP1 --primary --mode 1920x1080 --pos 1440x1432 --rotate normal
+xrandr --output DP1 --off
+xrandr --output HDMI2 --off
+xrandr --output HDMI1 --off
+xrandr --output DP1-3 --off
+xrandr --output DP1-2 --mode 2560x1440 --pos 2416x0 --rotate normal
+xrandr --output DP1-1 --mode 2560x1440 --pos 0x0 --rotate right --output DP2 --off
--- .screenlayout/hogg02.sh
+++ .screenlayout/hogg02.sh
@@ -1,2 +1,6 @@
 #!/bin/sh
-xrandr --output VIRTUAL1 --off --output eDP1 --primary --mode 1920x1080 --pos 0x272 --rotate normal --output DP1 --off --output HDMI2 --mode 1920x1080 --pos 1920x0 --rotate normal --output HDMI1 --off --output DP2 --off
+xrandr --output VIRTUAL1 --off
+xrandr --output eDP1 --primary --mode 1920x1080 --pos 0x272 --rotate normal
+xrandr --output DP1 --off
+xrandr --output HDMI2 --mode 1920x1080 --pos 1920x0 --rotate normal
+xrandr --output HDMI1 --off --output DP2 --off
