0b0995eb8a887c17b46f5e1bd1959760b34e7f48
Conrad <conrad.heidebrecht@gmail.com>
2019-03-19 10:23:25 -0400

    added natural scrolling to touchpad script

--- .config/i3/touchpad.py
+++ .config/i3/touchpad.py
@@ -11,6 +11,9 @@ if __name__ == '__main__':
     devices = run_cmd('xinput')
     dev_id = int([d.split('id=')[-1].split()[0] for d in devices if 'TouchPad' in d][0])
     props = run_cmd(f'xinput list-props {dev_id}')
-    prop_id = int([p.split('(')[-1].split(')')[0] for p in props if 'Tapping Enabled' in p and 'Default' not in p][0])
-    print(run_cmd(f'xinput set-prop {dev_id} {prop_id} 1'))
+    line_to_id = lambda s: s.split('(')[-1].split(')')[0]
+    tap_id = int([line_to_id(p) for p in props if 'Tapping Enabled' in p and 'Default' not in p][0])
+    scroll_id = int([line_to_id(p) for p in props if 'Natural Scrolling Enabled' in p and 'Default' not in p][0])
+    print(run_cmd(f'xinput set-prop {dev_id} {tap_id} 1'))
+    print(run_cmd(f'xinput set-prop {dev_id} {scroll_id} 1'))
 
